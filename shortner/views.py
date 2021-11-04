a41efcb6ab236311cdc3f488195e53e98a31aebd
Kobzpik <prabath.pik2299@gmail.com>
2021-11-04 13:52:52 +0530

    add createURL models

--- shortner/views.py
+++ shortner/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import ShortURL
+from .form import createNewURL
+from datetime import datetime
+import random,string
 
 # Create your views here.
 
@@ -14,5 +17,27 @@ def result(request, url):
     context = {'obj':current_obj[0]}
     return render(request, 'res.htm',context)
 
+#create new short URL
 def createShortURL(request):
-    pass
+    if request.method == 'POST':
+        form = createShortURL(request.POST) #Create new form
+        if form.is_valid():
+            original_website = form.cleaned_data['original_url']
+            random_chars_list = list(string.ascii_letters)
+            random_chars =''
+            for i in range(6):
+                random_chars += random.choice(random_chars_list)
+            while len(ShortURL.object.filter(short_url=random_chars)) !=0:
+                for i in range(6):
+                    random_chars += random.choice(random_chars_list)
+            
+            d=datetime.now()
+            s=ShortURL(original_url=original_website,short_url=random_chars,time_date_created=d)
+
+            s.save()
+            return render(request, 'urlcreated.htm',{'chars':random_chars})
+    
+    else:
+        form=createNewURL()
+        context={'form':form}
+        return render(request,'createurl.htm',context)
